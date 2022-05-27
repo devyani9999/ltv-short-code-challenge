194cf746aaace6593f65e79bb852744ee41b8a96
devyani9999 <devyani12034@iiitd.ac.in>
2022-05-28 03:24:57 +0530

    1. Algorithm to shorten the url and back to id, 2. Validation in place for full_url, 3. create method for short url implemented

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -7,9 +7,20 @@ class ShortUrlsController < ApplicationController
   end
 
   def create
+    short_url = ShortUrl.new(full_url: permitted_params_for_create['full_url'])
+    if short_url.save
+      render json: short_url.as_json, status: 200
+    else
+      render json: { errors: short_url.errors.full_messages.as_json }, status: 400
+    end
   end
 
   def show
   end
 
+  private
+
+  def permitted_params_for_create
+    params.permit(:full_url)
+  end
 end
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,10 +1,46 @@
 class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
+  CHARACTERS_MAPPING = Hash[CHARACTERS.zip(0...CHARACTERS.size)]
 
+  validates_presence_of :full_url
   validate :validate_full_url
 
+  after_save :update_short_url
+
   def short_code
+    code = []
+    uuid = self.id
+    unless uuid.nil?
+      while uuid > 0
+        code.append(CHARACTERS[uuid % 62])
+        uuid = uuid / 62
+      end
+
+      return code.join.reverse
+    end
+
+    nil
+  end
+
+  def self.id_from_short_code(short_code)
+    uuid = 0
+    code = short_code.split(//).reverse
+    code.each.with_index do |char, index|
+      value = CHARACTERS_MAPPING[char]
+      uuid = uuid + (value * (62 ** index))
+    end
+
+    uuid
+  end
+
+  def as_json(options = {})
+    json_hash = super(options)
+    json_hash.delete('id')
+    json_hash.delete('created_at')
+    json_hash.delete('updated_at')
+
+    json_hash
   end
 
   def update_title!
@@ -13,6 +49,18 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
+    return if self.full_url.blank?
+    begin
+      uri = URI.parse(self.full_url)
+      unless uri.is_a?(URI::HTTP) && !uri.host.nil?
+        self.errors.add(:full_url, "is not a valid url")
+      end
+    rescue
+      self.errors.add(:full_url, "is not a valid url")
+    end
   end
 
+  def update_short_url
+    self.update_columns(short_code: short_code)
+  end
 end
